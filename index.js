a590dd91e2dd0a0c1dab01fa32c6d6246490f8d6
Varenya0104 <119719253+Varenya0104@users.noreply.github.com>
2024-05-27 10:48:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
-// Add this after the other middleware
+
 const careerPathsRouter = require('./routes/careerPaths');
 app.use('/careerPaths', careerPathsRouter);
 
